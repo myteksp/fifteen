ae75a738854ba98f2b784f48ece568aabe35a47a
Dmitry Paradny <dmitry.paradny@gmail.com>
2017-05-28 13:05:28 +0300

    first commit

---
+++ src/main/java/com/gf/fifteen/FifteenApplication.java
@@ -0,0 +1,12 @@
+package com.gf.fifteen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FifteenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FifteenApplication.class, args);
+	}
+}
